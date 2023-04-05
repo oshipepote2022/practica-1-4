d54e68d24750085938bd2f0ee4729451b1329a3b
Gustavo <gustavo_r69@yahoo.com.ar>
2023-04-05 19:15:40 -0300

    comienzo fin practica 1-4

---
+++ algoritmo.js
@@ -0,0 +1,12 @@
+document.querySelector().innerHTML= '
+<details>
+        <summary>Titulo de informacion importante</summary>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNzuq37ZmH5UtLmLozf_YjqjdrZYWSImd1EsIcbTP1Tw&s "
+            alt=" dibujitos">
+        <iframe src="https://www.consejo.org.ar/ frameborder="0"></iframe>
+        <p>Ayrton Senna da Silva fue un piloto de automovilismo brasileño. Siendo tres veces campeón mundial de Fórmula
+            1, dos veces subcampeón y una vez tercero, figura entre los más exitosos y dominantes pilotos de la era
+            moderna, y, para muchos expertos, es el más rápido de la historia​</p>
+        <a href="https://www.youtube.com/watch?v=CI_arNEolkY">ver video</a>
+    </details>
+';
